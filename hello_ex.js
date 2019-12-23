7d140501d1387463ef914b63d365261adce001f8
TakashiSaitou <bookman2710@gmail.com>
2019-12-23 21:40:23 +0900

    色々と変えた

--- hello_ex.js
+++
@@ -1,6 +0,0 @@
-let book = { title: 'javaポケットリファレンス', publish: '技術評論社', price: 2680, other: {keyword: 'java', logo: 'logo.jpg'} };
-let {title, other, other: {keyword, logo} } = book;
-console.log(title);
-console.log(other);
-console.log(keyword);
-console.log(book);
